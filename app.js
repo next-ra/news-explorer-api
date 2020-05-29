72fa32de5c78164361e6dc9cb7a1b54807075bbc
Denis Luchnikov <lasvegas3600@gmail.com>
2020-05-29 07:52:41 +0300

    Initial local

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
+const config = require('./config');
+
+const app = express();
+
+app.use(routes);
+mongoose
+  .connect(config.MONGODB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('База данных подключена'))
+  .catch(() => console.log('Ошибка подключения к базе данных'));
+
+app.listen(config.PORT, () => {
+  console.log(`Сервер работает на ${config.PORT} порту`);
+  console.log('NODE_ENV:', process.env.NODE_ENV);
+});
---
+++ models/user.js
@@ -0,0 +1,3 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const uniqueValidator = require('mongoose-unique-validator');
