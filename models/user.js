44ad453353173a9a40e36e9101ddfe6b6674ec5c
Denis Luchnikov <lasvegas3600@gmail.com>
2020-05-29 08:15:22 +0300

    add user model

--- models/user.js
+++ models/user.js
@@ -1,3 +1,28 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const uniqueValidator = require('mongoose-unique-validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: [true, 'обязательное поле']
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'обязательное поле'],
+    validate: v => validator.isEmail(v),
+    message: 'неправильный формат почты'
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    select: false
+  }
+});
+
+userSchema.plugin(uniqueValidator);
+module.exports = mongoose.model('user', userSchema);
