aa4c68fef107ea8b1174b847432e0e809996d9da
Denis Luchnikov <lasvegas3600@gmail.com>
2020-05-29 20:42:10 +0300

    Add logger

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,16 @@ const { createUser, login } = require('../controllers/users');
 const { errorHandler } = require('../middlewares/errorHandler');
 const usersRouter = require('./users');
 const { auth } = require('../middlewares/auth');
+const { requestLogger, errorLogger } = require('../middlewares/logger');
 
 router.use(cookieParser());
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
-
+router.use(requestLogger);
 router.post('/signup', createUser);
 router.post('/signin', login);
 router.use(auth);
 router.use('/users', usersRouter);
-
+router.use(errorLogger);
 router.use('/', errorHandler);
 module.exports = router;
